7d000dc1c93904d53f5bcd3aa256ed2892ab1654
Petri Kero <petrikero2@gmail.com>
2025-07-21 14:42:41 +0300

    Add docs/install.sh to enable downloading the install script via GitHub Pages which doesn't have the download request throttling.

---
+++ docs/install.sh
@@ -0,0 +1,7 @@
+---
+# Alias for the install.sh script (in the repo root) to enable publishing via GitHub Pages which
+# doesn't have the 60 requests/hour/IP throttling that raw.githubusercontent.com has.
+# Use https://metaplay.github.io/cli/install.sh to download install.sh via Pages.
+layout: null
+---
+{% include_relative ../install.sh %}
